6a3fc1900688562cff92faa027ab8dc84d9c35b1
Renjith <renjith.r@outlook.com>
2016-07-25 20:31:35 +0530

    Added new UI

--- CricTrac/CricTrac/Controller/NewMatchViewController.swift
+++ CricTrac/CricTrac/Controller/NewMatchViewController.swift
@@ -10,55 +10,43 @@ import UIKit
 
 class NewMatchViewController: UIViewController {
 
-    @IBOutlet weak var dateTest: UITextField!
+    @IBOutlet weak var dateTest: FloatLabelTextField!
     
     @IBOutlet weak var datePickerButton:UIButton!
     
-    @IBOutlet weak var dateLabel: UILabel!
     
     @IBOutlet weak var teamText: UITextField!
     
-    @IBOutlet weak var teamLabel: UILabel!
     
     @IBOutlet weak var opponentText: UITextField!
     
-    @IBOutlet weak var opponentLabel: UILabel!
     
     @IBOutlet weak var groundText: UITextField!
     
-    @IBOutlet weak var groundLabel: UILabel!
     
     @IBOutlet weak var OversText: UITextField!
     
-    @IBOutlet weak var OversLabel: UILabel!
     
     @IBOutlet weak var tournamnetText: UITextField!
     
-    @IBOutlet weak var tournamentLabel: UILabel!
     
     @IBOutlet weak var dismissText: UITextField!
     
-    @IBOutlet weak var dismissLabel: UILabel!
     
     
     @IBOutlet weak var extraOverText: UITextField!
     
-    @IBOutlet weak var extraOverLabel: UILabel!
     
     @IBOutlet weak var wicketsText: UITextField!
     
-    @IBOutlet weak var wicketLabel: UILabel!
     
     @IBOutlet weak var resultsText: UITextField!
     
-    @IBOutlet weak var resultsLabel: UILabel!
     
     
-    @IBOutlet weak var commentsLabel: UILabel!
     
     @IBOutlet weak var commentsText: UITextView!
     
-    @IBOutlet weak var matchWonSwitch: UISwitch!
     
     
     @IBOutlet weak var matchSelector: UIView!
@@ -75,7 +63,9 @@ class NewMatchViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         lastSelectedTab = matchSelector
-       
+
+        
+        
         // Do any additional setup after loading the view.
     }
 
@@ -95,25 +85,8 @@ class NewMatchViewController: UIViewController {
         matchSelector.hidden = false
         lastSelectedTab?.hidden = true
         lastSelectedTab = matchSelector
-        
-        datePickerButton.hidden = false
-        dateLabel.text = "Date:"
-        teamLabel.text = "Team:"
-        opponentLabel.text = "Opponent:"
-        groundLabel.text = "Ground:"
-        OversLabel.text = "Overs:"
-        tournamentLabel.text = "Tournament:"
-        dismissLabel.hidden = true
-        dismissText.hidden = true
-        
-        extraOverLabel.hidden = true
-        extraOverText.hidden = true
-        wicketLabel.hidden = true
-        resultsLabel.hidden = true
-        resultsText.hidden = true
-        matchWonSwitch.hidden = true
-        commentsText.hidden = true
-        commentsLabel.hidden = true
+
+          dateTest.placeholder = "Date"
     }
   
     
@@ -122,24 +95,7 @@ class NewMatchViewController: UIViewController {
         lastSelectedTab?.hidden = true
         lastSelectedTab = battingSelector
         
-        datePickerButton.hidden = true
-        dateLabel.text = "Runs:"
-        teamLabel.text = "Balls:"
-        opponentLabel.text = "4S:"
-        groundLabel.text = "6S:"
-        OversLabel.text = "Strike Rate:"
-        tournamentLabel.text = "Position:"
-        dismissLabel.hidden = false
-        dismissText.hidden = false
-        
-        extraOverLabel.hidden = true
-        extraOverText.hidden = true
-        wicketLabel.hidden = true
-        resultsLabel.hidden = true
-        resultsText.hidden = true
-        matchWonSwitch.hidden = true
-        commentsText.hidden = true
-        commentsLabel.hidden = true
+        dateTest.placeholder = "Balls"
     }
     
     
@@ -148,24 +104,7 @@ class NewMatchViewController: UIViewController {
         lastSelectedTab?.hidden = true
         lastSelectedTab = bowlingSelector
         
-        datePickerButton.hidden = true
-        dateLabel.text = "Overs:"
-        teamLabel.text = "Wickets:"
-        opponentLabel.text = "Runs:"
-        groundLabel.text = "No Balls:"
-        OversLabel.text = "Wides:"
-        tournamentLabel.text = "Economy:"
-        dismissLabel.hidden = true
-        dismissText.hidden = true
-        
-        extraOverLabel.hidden = true
-        extraOverText.hidden = true
-        wicketLabel.hidden = true
-        resultsLabel.hidden = true
-        resultsText.hidden = true
-        matchWonSwitch.hidden = true
-        commentsText.hidden = true
-        commentsLabel.hidden = true
+
     }
     
     
@@ -174,24 +113,7 @@ class NewMatchViewController: UIViewController {
         lastSelectedTab?.hidden = true
         lastSelectedTab = extraSelector
         
-        datePickerButton.hidden = true
-        dateLabel.text = "Toss:"
-        teamLabel.text = "First Batting:"
-        opponentLabel.text = "Score:"
-        groundLabel.text = "Over:"
-        OversLabel.text = "Wickets:"
-        tournamentLabel.text = "Second Batting:"
-        dismissLabel.hidden = false
-        dismissText.hidden = false
-        dismissLabel.text = "Score"
-        extraOverLabel.hidden = false
-        extraOverText.hidden = false
-        wicketLabel.hidden = false
-        resultsLabel.hidden = false
-        resultsText.hidden = false
-        matchWonSwitch.hidden = false
-        commentsText.hidden = false
-        commentsLabel.hidden = false
+   
         
     }
     
--- CricTrac/Pods/Target Support Files/Pods-CricTrac/Pods-CricTrac-frameworks.sh
+++ CricTrac/Pods/Target Support Files/Pods-CricTrac/Pods-CricTrac-frameworks.sh
@@ -87,9 +87,11 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/CSStickyHeaderFlowLayout/CSStickyHeaderFlowLayout.framework"
   install_framework "$BUILT_PRODUCTS_DIR/IOStickyHeader/IOStickyHeader.framework"
   install_framework "$BUILT_PRODUCTS_DIR/KYDrawerController/KYDrawerController.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/SkyFloatingLabelTextField/SkyFloatingLabelTextField.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/CSStickyHeaderFlowLayout/CSStickyHeaderFlowLayout.framework"
   install_framework "$BUILT_PRODUCTS_DIR/IOStickyHeader/IOStickyHeader.framework"
   install_framework "$BUILT_PRODUCTS_DIR/KYDrawerController/KYDrawerController.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/SkyFloatingLabelTextField/SkyFloatingLabelTextField.framework"
 fi
