be29f2d97e4b3a347578aa3c78f8defef9bcffeb
Renjith <renjith.r@outlook.com>
2016-07-29 06:37:47 +0530

    Menu added

---
+++ CricTrac/CricTrac/Other/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  CricTrac
+//
+//  Created by Renjith on 7/28/16.
+//  Copyright © 2016 CricTrac. All rights reserved.
+//
+
+import Foundation
+
+let menuData = [
+
+    ["title":"New Match","vc":"NewMatchViewController"],
+    ["title":"Summary","vc":"SummaryMatchDetailsViewController"],
+    ["title":"Summary Match Details","vc":"SummaryViewController"]
+]
