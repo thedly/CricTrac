6a3594bff217b271348dfdf73f531d248f434784
Tejas Hedly <tj.werewolf@gmail.com>
2016-07-31 10:49:24 +0530

    Completed basic profile page

--- CricTrac/CricTrac/ProfileViewController.swift
+++ CricTrac/CricTrac/ProfileViewController.swift
@@ -8,17 +8,22 @@
 
 import UIKit
 
-class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITableViewDelegate, UITableViewDataSource {
 
     lazy var ctDatePicker = CTDatePicker()
     
     @IBOutlet weak var scrollView:UIScrollView!
     var imgPicker = UIImagePickerController()
     
+    @IBOutlet weak var noDataView: UIView!
     
     @IBOutlet weak var miscTblView: UITableView!
     
     @IBOutlet weak var profileImg: UIImageView!
+    
+    var teamdata: [String]!
+    var grounddata: [String]!
+    var opponentdata: [String]!
     
     @IBOutlet weak var dateTest: UITextField!
     
@@ -59,26 +64,35 @@ class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, 
     
     
     
-    @IBOutlet weak var matchSelector: UIView!
+    @IBOutlet weak var personalSelector: UIView!
     
-    @IBOutlet weak var bowlingSelector: UIView!
+    @IBOutlet weak var teamSelector: UIView!
+    
+   
+    @IBOutlet weak var groundSelector: UIView!
     
     
-    @IBOutlet weak var extraSelector: UIView!
-    
-    @IBOutlet weak var battingSelector: UIView!
-    
+    @IBOutlet weak var opponentSelector: UIView!
     
     var lastSelectedTab:UIView?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        lastSelectedTab = matchSelector
-        imgPicker.delegate = self
-        scrollView.setContentOffset(CGPointZero, animated: true)
         
         
+        initializeView()
         // Do any additional setup after loading the view.
+    }
+    
+    func initializeView(){
+        lastSelectedTab = personalSelector
+        imgPicker.delegate = self
+        miscTblView.dataSource = self
+        miscTblView.delegate = self
+        scrollView.setContentOffset(CGPointZero, animated: true)
+        profileImg.layer.cornerRadius = 10
+        profileImg.clipsToBounds = true
+        getMiscDetails()
     }
     
     override func didReceiveMemoryWarning() {
@@ -108,39 +122,107 @@ class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, 
     @IBAction func didTapPersonal(sender: AnyObject) {
         
         scrollView.setContentOffset(CGPointZero, animated: true)
+        lastSelectedTab = personalSelector
        miscTblView.hidden = true
         scrollView.hidden = false
+        noDataView.hidden = !scrollView.hidden
+        hideAllSelectors()
+        personalSelector.hidden = false
+        miscTblView.reloadData()
     }
     
     
     @IBAction func didTapTeam(sender: AnyObject){
         scrollView.setContentOffset(CGPointZero, animated: true)
-        miscTblView.hidden = false
+        miscTblView.hidden = teamdata.count == 0
+
         scrollView.hidden = true
+        noDataView.hidden = !miscTblView.hidden
+        lastSelectedTab = teamSelector
+        hideAllSelectors()
+        teamSelector.hidden = false
+        miscTblView.reloadData()
     }
     
     
     @IBAction func didTapGround(sender: AnyObject) {
         scrollView.setContentOffset(CGPointZero, animated: true)
-        miscTblView.hidden = false
+        miscTblView.hidden = grounddata.count == 0
         scrollView.hidden = true
-        
+        noDataView.hidden = !miscTblView.hidden
+        lastSelectedTab = groundSelector
+        hideAllSelectors()
+        groundSelector.hidden = false
+        miscTblView.reloadData()
     }
     
     
     @IBAction func didTapOpponent(sender: AnyObject) {
         scrollView.setContentOffset(CGPointZero, animated: true)
-        miscTblView.hidden = false
+        miscTblView.hidden = opponentdata.count == 0
         scrollView.hidden = true
+        noDataView.hidden = !miscTblView.hidden
+        lastSelectedTab = opponentSelector
+        hideAllSelectors()
+        opponentSelector.hidden = false
+        miscTblView.reloadData()
     }
     
     func hideAllSelectors(){
-        matchSelector.hidden = true
-        battingSelector.hidden = true
-        bowlingSelector.hidden = true
-        extraSelector.hidden = true
+        personalSelector.hidden = true
+        teamSelector.hidden = true
+        opponentSelector.hidden = true
+        groundSelector.hidden = true
     }
     
+    
+    
+    
+    // MARk: - Table delegate functions
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch lastSelectedTab! {
+        case opponentSelector:
+            return opponentdata.count
+        case teamSelector:
+            return teamdata.count
+        case groundSelector:
+            return grounddata.count
+        default:
+            return 0
+        }
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        
+        switch lastSelectedTab! {
+        case groundSelector:
+            cell.textLabel?.text = grounddata[indexPath.row]
+        case teamSelector:
+            cell.textLabel?.text = teamdata[indexPath.row]
+        case opponentSelector:
+            cell.textLabel?.text = opponentdata[indexPath.row]
+        default:
+            break
+        }
+        return cell
+    }
+    func tableView(tableView: UITableView, editActionsForRowAtIndexPath indexPath: NSIndexPath) -> [UITableViewRowAction]? {
+        let edit = UITableViewRowAction(style: .Normal, title: "Edit") { action, index in
+            print("edit button tapped")
+        }
+        edit.backgroundColor = UIColor.lightGrayColor()
+        let delete = UITableViewRowAction(style: .Normal, title: "Delete") { action, index in
+            print("delete button tapped")
+        }
+        delete.backgroundColor = UIColor.redColor()
+        return [delete, edit]
+    }
     
 }
 
@@ -164,7 +246,13 @@ extension ProfileViewController:UITextFieldDelegate{
         return true
     }
     
+    // MARK: - Service calls
     
     
+    func getMiscDetails() {
+        teamdata = ["JOJO", "DPS", "INHS"]
+        grounddata = ["D-Pitch, Indiranagar", "St.John's college, Koramangala "]
+        opponentdata = []
+    }
 
 }
