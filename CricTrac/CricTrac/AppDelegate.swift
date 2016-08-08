230e87d3b0734eabe1dcfaab014037642360f68b
Tejas Hedly <tj.werewolf@gmail.com>
2016-08-08 21:05:23 +0530

    Added parallaxVC. old POC for dashboard

--- CricTrac/CricTrac/AppDelegate.swift
+++ CricTrac/CricTrac/AppDelegate.swift
@@ -86,7 +86,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
     
     func setSliderMenu(){
-        let dashboardVC = viewControllerFrom("Main", vcid: "DashboardViewController") as! DashboardViewController
+        let dashboardVC = viewControllerFrom("Main", vcid: "parallaxVC") as! parallaxVC
 
         window = UIWindow(frame: UIScreen.mainScreen().bounds)
         window?.makeKeyAndVisible()
