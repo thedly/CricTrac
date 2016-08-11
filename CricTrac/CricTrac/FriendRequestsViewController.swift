def3cc376193bc9108f0dfeeb749e956e19e2a38
Tejas Hedly <tj.werewolf@gmail.com>
2016-08-11 22:40:41 +0530

    friends module base created

---
+++ CricTrac/CricTrac/FriendRequestsViewController.swift
@@ -0,0 +1,78 @@
+//
+//  FriendRequestsViewController.swift
+//  CricTrac
+//
+//  Created by Tejas Hedly on 11/08/16.
+//  Copyright © 2016 CricTrac. All rights reserved.
+//
+
+import UIKit
+import XLPagerTabStrip
+
+class FriendRequestsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, IndicatorInfoProvider {
+
+    @IBOutlet weak var RequestsTblview: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        initializeView()
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - Methods
+    
+    func initializeView() {
+        RequestsTblview.dataSource = self
+        RequestsTblview.delegate = self
+        RequestsTblview.allowsSelection = false
+    }
+    
+    func indicatorInfoForPagerTabStrip(pagerTabStripController: PagerTabStripViewController) -> IndicatorInfo {
+        return IndicatorInfo(title: "REQUESTS")
+    }
+    
+    
+    // MARK: - Table delegate functions
+    
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCellWithIdentifier("FriendRequestsTableViewCell", forIndexPath: indexPath) as? FriendRequestsTableViewCell {
+            
+            let friendProfileName = "Sajith"
+            let friendProfileTeam = "DPS, Bangalore"
+            
+            cell.configureCell(friendProfileName, friendTeamName: friendProfileTeam, friendProfileImage: "")
+            return cell
+        }
+        else
+        {
+            return FriendRequestsTableViewCell()
+        }
+    }
+    
+   
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ CricTrac/CricTrac/FriendSearchViewController.swift
@@ -0,0 +1,40 @@
+//
+//  FriendSearchViewController.swift
+//  CricTrac
+//
+//  Created by Tejas Hedly on 11/08/16.
+//  Copyright © 2016 CricTrac. All rights reserved.
+//
+
+import UIKit
+import XLPagerTabStrip
+
+class FriendSearchViewController: UIViewController,IndicatorInfoProvider {
+
+    @IBOutlet weak var friendSearchTbl: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func indicatorInfoForPagerTabStrip(pagerTabStripController: PagerTabStripViewController) -> IndicatorInfo {
+        return IndicatorInfo(title: "SEARCH")
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
