8ee2361b1c33b37fecc1550cda45a956d5ab5d40
Sajith Kumar <sajithkumarn@gmail.com>
2017-03-23 15:31:31 +0530

    changes

--- CricTrac/CricTrac/AddMatchDetailsViewController.swift
+++ CricTrac/CricTrac/AddMatchDetailsViewController.swift
@@ -399,9 +399,10 @@ class AddMatchDetailsViewController: ButtonBarPagerTabStripViewController,MatchP
      */
     func moveToMatchSummary()  {
         
-        //let dashboardVC = viewControllerFrom("Main", vcid: "MatchSummaryViewController") as! MatchSummaryViewController
-        //let navigationControl = UINavigationController(rootViewController: dashboardVC)
-        //sliderMenu.mainViewController = navigationControl
-        self.navigationController?.popViewControllerAnimated(true)
+        let dashboardVC = viewControllerFrom("Main", vcid: "MatchSummaryViewController") as! MatchSummaryViewController
+        let navigationControl = UINavigationController(rootViewController: dashboardVC)
+        sliderMenu.mainViewController = navigationControl
+        //self.navigationController?.popToRootViewControllerAnimated(true)
+        //self.navigationController?.popViewControllerAnimated(true)
     }
 }
