b3ecf757828de6cabea61e5d91573f261ebe8fbf
sravani <sravani.mdr@gmail.com>
2017-04-24 18:40:43 +0530

    Profile changes

--- CricTrac/CricTrac/SettingsViewController.swift
+++ CricTrac/CricTrac/SettingsViewController.swift
@@ -110,7 +110,7 @@ class SettingsViewController: UIViewController, UITableViewDelegate, UITableView
         cell.menuItemSelectedValue.text = selectedValue
         
         
-        cell.menuItemToggleSwitch.hidden = !toggleConfig
+      //  cell.menuItemToggleSwitch.hidden = !toggleConfig
         cell.menuItemSelectedValue.hidden = toggleConfig
         
         
