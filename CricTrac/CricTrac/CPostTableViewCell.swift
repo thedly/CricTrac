1f35468ce203cde8fcdef9f62f139f2caad0c048
Sajith Kumar <sajithkumarn@gmail.com>
2017-05-02 11:34:06 +0530

    Comments TableViewCell XIB file

---
+++ CricTrac/CricTrac/CPostTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CPostTableViewCell.swift
+//  CricTrac
+//
+//  Created by AIPL on 02/05/17.
+//  Copyright © 2017 CricTrac. All rights reserved.
+//
+
+import UIKit
+
+class CPostTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
