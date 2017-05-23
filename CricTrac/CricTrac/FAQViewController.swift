86e6a4fd2d4032125e89c5911d21e6656b70c4c0
sravani <sravani.mdr@gmail.com>
2017-05-23 19:49:49 +0530

    FAQVC

--- CricTrac/CricTrac/FAQViewController.swift
+++ CricTrac/CricTrac/FAQViewController.swift
@@ -16,7 +16,7 @@ class FAQViewController: UIViewController,ThemeChangeable {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
+        //set
         setBackgroundColor()
         
     }
