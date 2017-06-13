953d8decb1438148d47474d4a3f86343ea4768c3
Sajith Kumar <sajithkumarn@gmail.com>
2017-06-13 19:14:14 +0530

    Production build v1.1

--- CricTrac/CricTrac/Other/Constants.swift
+++ CricTrac/CricTrac/Other/Constants.swift
@@ -137,9 +137,9 @@ var adUnitId = ""
 let fireBaseRef =  FIRDatabase.database().reference()
 
 //Dev File Storage
-let storageRef = FIRStorage.storage().referenceForURL("gs://arjun-innovations.appspot.com")
+//let storageRef = FIRStorage.storage().referenceForURL("gs://arjun-innovations.appspot.com")
 //Azure Dev environment url
-let serverBaseURL = "https://crictracdevserver.azurewebsites.net"
+//let serverBaseURL = "https://crictracdevserver.azurewebsites.net"
 
 //QA File Storage
 //let storageRef = FIRStorage.storage().referenceForURL("gs://crictracweb.appspot.com")
@@ -147,16 +147,16 @@ let serverBaseURL = "https://crictracdevserver.azurewebsites.net"
 //let serverBaseURL = "https://crictracqaserver.azurewebsites.net"
 
 //Prod File Storgage
-//let storageRef = FIRStorage.storage().referenceForURL("gs://crictrac-2e4c7.appspot.com")
+let storageRef = FIRStorage.storage().referenceForURL("gs://crictrac-2e4c7.appspot.com")
 //Azure Prod environment url
-//let serverBaseURL = "https://crictracserver.azurewebsites.net"
+let serverBaseURL = "https://crictracserver.azurewebsites.net"
 
-var versionAndBuildNumber:String{
-    let nsObject: AnyObject? = NSBundle.mainBundle().infoDictionary!["CFBundleShortVersionString"]
-    let build = NSBundle.mainBundle().objectForInfoDictionaryKey(kCFBundleVersionKey as String) as! String
-    return  (nsObject as! String) + "(" + build + ")"
-}
-//var versionAndBuildNumber = "1.0"
+//var versionAndBuildNumber:String{
+//    let nsObject: AnyObject? = NSBundle.mainBundle().infoDictionary!["CFBundleShortVersionString"]
+//    let build = NSBundle.mainBundle().objectForInfoDictionaryKey(kCFBundleVersionKey as String) as! String
+//    return  (nsObject as! String) + "(" + build + ")"
+//}
+var versionAndBuildNumber = "1.1"
 
  struct CustomTextInputStyle: AnimatedTextInputStyle  {
     var activeColor =   UIColor(hex: "#B12420")
