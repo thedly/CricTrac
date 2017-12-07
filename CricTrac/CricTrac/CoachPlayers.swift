15bd8ee37782ed9813f93f9be9e40ed40a049b05
Sravani <sravani.mdr@gmail.com>
2017-12-07 11:16:21 +0530

    New class - CoachPlayers

---
+++ CricTrac/CricTrac/CoachPlayers.swift
@@ -0,0 +1,18 @@
+//
+//  CoachPlayers.swift
+//  CricTrac
+//
+//  Created by AIPL on 05/12/17.
+//  Copyright © 2017 CricTrac. All rights reserved.
+//
+
+import Foundation
+
+class CoachPlayers {
+    
+    var dob: NSDate!
+    var myPlayersId: String!
+    var playerName: String!
+    var playerRole: String!
+    var city: String!
+}
