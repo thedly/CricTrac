2856240edbd61501412e8af04bb52aca13a82e6f
Sravani <aipl@Arjuns-MacBook-Pro.local>
2018-01-29 11:38:26 +0530

    NewFile created - SummaryMatchDetailsViewController

---
+++ CricTrac/CricTrac/SummaryMatchDetailsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SummaryMatchDetailsViewController.swift
+//  CricTrac
+//
+//  Created by Arjun Innovations on 29/01/18.
+//  Copyright © 2018 CricTrac. All rights reserved.
+//
+
+import UIKit
+
+class SummaryMatchDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
